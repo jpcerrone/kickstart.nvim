fdffb380d5d1ca350809f07accc6bfb3d25deaef
jpcerrone <jpcerronex@gmail.com>
2025-06-19 23:21:47 -0300

    custom snippets setup

--- init.lua
+++ init.lua
@@ -788,6 +788,7 @@ require('lazy').setup({
             'rafamadriz/friendly-snippets',
             config = function()
               require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_vscode').lazy_load { paths = '~/.config/nvim/snippets' }
             end,
           },
         },
